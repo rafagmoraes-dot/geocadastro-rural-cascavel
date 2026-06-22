e7edc65568796dbb65121e5e619d9a080fa72fb7
rafagmoraes-dot <rafagmoraes@outlook.com>
2026-06-22 02:30:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def carregar_dados():
     cascavel = gpd.read_file("cascavel.geojson")
     tabela = pd.read_csv(URL_PLANILHA)
 
-    gdf_geo["id"] = gdf_geo["id"].astype(int).astype(str)
+    gdf_geo["id"] = gdf_geo["id"].astype(float).astype(int).astype(str)
     tabela["id"] = tabela["id"].astype(float).astype(int).astype(str)
 
     gdf = gdf_geo[["id", "geometry"]].merge(
@@ -29,6 +29,8 @@ def carregar_dados():
         on="id",
         how="left"
     )
+
+    gdf["id"] = gdf["id"].astype(float).astype(int).astype(str)
 
     gdf = gpd.GeoDataFrame(gdf, geometry="geometry", crs=gdf_geo.crs)
 
